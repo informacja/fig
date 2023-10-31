871340f70203478f70fc095ad2de04bb101d9e6c
Piotr Wawryka <piotr@student.agh.edu.pl>
2023-10-31 21:19:55 +0100

    Create overwrite.m

---
+++ overwrite.m
@@ -0,0 +1,15 @@
+### Library update
+Copy and paste this in your MATLAB console to update  
+```matlab
+proj_path = pwd;
+cd(fullfile(userpath));
+urlwrite ('https://raw.githubusercontent.com/informacja/fig/main/startup.m', 'startup.m');
+ urlwrite ('https://raw.githubusercontent.com/informacja/fig/main/figPSW.m', 'figPSW.m');
+  urlwrite ('https://raw.githubusercontent.com/informacja/fig/main/figPW.m', 'figPW.m');
+   urlwrite ('https://raw.githubusercontent.com/informacja/fig/main/figP.m', 'figP.m');
+urlwrite ('https://raw.githubusercontent.com/informacja/fig/main/eps2pdf.m', 'eps2pdf.m');
+ urlwrite ('https://raw.githubusercontent.com/informacja/fig/main/finder.m', 'finder.m');
+startup;
+cd(proj_path);
+clc; fprintf(1,'Now you can type here "help figPW".\nIf you want save all opened figures just run "figPSW".\n')
+```
