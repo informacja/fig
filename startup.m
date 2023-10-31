776390a4cf22d70977d99510fc8d85765446e431
Piotr Wawryka <piotrwpwsz@gmail.com>
2023-10-31 21:07:38 +0100

    Update startup.m

--- startup.m
+++ startup.m
@@ -6,15 +6,15 @@ cd(fullfile(userpath));
 
 % TODO try catch, webwrite
 if ~isfile('figPSW.m')
-    urlwrite ('https://github.com/informacja/fig/blob/main/figPSW.m', 'figPSW.m')
+    urlwrite ('https://raw.githubusercontent.com/informacja/fig/main/figPSW.m', 'figPSW.m')
 end
 
 if ~isfile('figPW.m')
-    urlwrite ('https://github.com/informacja/fig/blob/main/figPW.m', 'figPW.m')
+    urlwrite ('https://raw.githubusercontent.com/informacja/fig/main/figPW.m', 'figPW.m')
 end
 
 if ~isfile('figP.m')
-    urlwrite ('https://github.com/informacja/fig/blob/main/figP.m', 'figP.m')
+    urlwrite ('https://raw.githubusercontent.com/informacja/fig/main/figP.m', 'figP.m')
 end
 
 addpath(fullfile(userpath)) 
